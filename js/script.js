ac2e78abcaf4364682d11b5af64fa69b5cd0aa94
cranderveldt <djnaphouse@gmail.com>
2014-02-21 20:26:14 -0500

    some basic status stuff

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ jQuery(document).ready(function($) {
     } else {
       $('#view-1').show().siblings().hide();
     }
+    $('.status-dark').show();
     $('#loader').hide();
   });
 });
@@ -14,57 +15,102 @@ var Main = function ($scope) {
   $scope.views = [
     {
       id: 1
+      , status: [
+        'dark'
+        , 'light'
+      ]
       , nav: {
         up: 6
         , right: 0
         , down: 0
         , left: 2
       }
+      , elms: [
+        {
+          top: '20px'
+          , left: '93px'
+          , width: '268px'
+          , height: '195px'
+          , type: 'message'
+          , message: 'It\'s a serene painting of mist coming off the mountains in Japan.'
+          , status: 'light'
+        }
+      ]
     }
     , {
       id: 2
+      , status: [
+        'dark'
+      ]
       , nav: {
         up: 0
         , right: 1
         , down: 0
         , left: 3
       }
+      , elms: [
+
+      ]
     }
     , {
       id: 3
+      , status: [
+        'dark'
+      ]
       , nav: {
         up: 0
         , right: 2
         , down: 0
         , left: 4
       }
+      , elms: [
+
+      ]
     }
     , {
       id: 4
+      , status: [
+        'dark'
+      ]
       , nav: {
         up: 0
         , right: 3
         , down: 0
         , left: 0
       }
+      , elms: [
+
+      ]
     }
     , {
       id: 5
+      , status: [
+        'dark'
+      ]
       , nav: {
         up: 0
         , right: 0
         , down: 0
         , left: 6
       }
+      , elms: [
+
+      ]
     }
     , {
       id: 6
+      , status: [
+        'dark'
+      ]
       , nav: {
         up: 0
         , right: 5
         , down: 0
         , left: 1
       }
+      , elms: [
+
+      ]
     }
   ];
 };
@@ -80,6 +126,7 @@ app.directive('ldChangeScreen', function () {
       element.on('click', function(e) {
         e.preventDefault();
         if (attrs.ldChangeScreen > 0 && attrs.ldChangeScreen <= scope.views.length) {
+          $('#message').text('');
           $('#view-' + attrs.ldChangeScreen).show().siblings().hide();
           $.cookie('current_view', attrs.ldChangeScreen, { expires: 30, path: '/' });
         }
@@ -87,4 +134,48 @@ app.directive('ldChangeScreen', function () {
     }
   };
 });
+app.directive('ldElement', function () {
+  return {
+    replace: false,
+    transclude: false,
+    restrict: 'A',
+    scope: false,
+    link: function (scope, element, attrs) {
+      element.on('click', function(e) {
+        e.preventDefault();
+        var data = attrs.$$element.context.dataset;
+        if (data.message !== '') {
+          $('#message').text(data.message);
+        }
+        if (data.status !== '') {
+          element.parent().parent().find('.status-' + data.status).show().siblings().hide();
+        }
+      });
+    }
+  };
+});
+
+
+/*
+
+
+open bathroom {
+  turn on water
+  turn on lights
+  turn on blender
+  turn on music {
+  
+  }
+
+}
+
+escape out the front door
+
+reveal yourself to people
+
+find and destroy spirit tether
+
+
+
+*/
 
